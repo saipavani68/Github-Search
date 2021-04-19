cbc33a5a70a25e7ac7898cfceec3dcd940905a3a
saipavani68 <nagisettipavani@gmail.com>
2021-04-18 23:26:10 -0700

    click of each user card

--- src/user-details-container.js
+++ src/user-details-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {browserHistory} from "react-router-dom";
 import axios from 'axios';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from 'react-bootstrap/Card';
@@ -13,6 +14,7 @@ export default class UserDetailsContainer extends Component {
             userData: [],
             isLoading: true
         };
+        this.cardClicked = this.cardClicked.bind(this);
     }
 
     componentDidMount(){
@@ -24,9 +26,21 @@ export default class UserDetailsContainer extends Component {
           .then(res => {
             const userData = res.data;
             this.setState({ userData, isLoading: false });
+            this.getFollowerCount(userData);
           });
           this.setState({ isLoading: true })
-        }    
+        }   
+        
+    /* getFollowerCount(userData) {
+        const users = userData.items
+        users.forEach((user, index) => {
+            axios.get(user.followers_url)
+                .then(res => {
+                const followersData = res.data;
+                this.setState({ ...this.state.userData.items, followersCount: followersData.length})
+                });
+            });
+        } */
     
     handlePageChange(pageNumber) {
         this.setState({activePage: pageNumber}, () => {
@@ -34,27 +48,28 @@ export default class UserDetailsContainer extends Component {
         });
     }
 
-    cardClicked(){
-        console.log("inside card clicked");
+    cardClicked(url){
+        window.location.replace(url);
     }
 
     render() {
         const { userData } = this.state
         const users = userData && userData.items
         const checkIfUsersNotEmpty = users && users.length > 0 
+
           return (
             <div className="users-details-container">
                 { !this.state.isLoading ? 
                 <div>
                     { checkIfUsersNotEmpty && <h3 className="total-users-text"> { userData.total_count } users </h3> }
                     { checkIfUsersNotEmpty &&
-                        users.map((user, index) => (
-                        <Card style={{ width: 600, left: 30 }} key={ index } onClick={ this.cardClicked }>
+                        users.map(user => (
+                        <Card style={{ width: 600, left: 30 }} key={ user.id } onClick={ () => this.cardClicked(user.html_url) }>
                         <Card.Body>  
                             <Card.Img src={ user.avatar_url } />
                             <Card.Text>
                                 <span className="username-header"> {user.login } </span>
-                                {user.id}
+                                { user.id }
                             </Card.Text>
                         </Card.Body>
                         </Card>
